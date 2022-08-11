7603154b453e6c9be50c7916f5f4ef10d88875a1
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-11 23:03:51 +0530

    Function Parameters

--- Basics/src/Main.kt
+++ Basics/src/Main.kt
@@ -2,13 +2,18 @@
 //    return "This is a kotlin function"
 //}
 
-fun getGreeting(): String = "Hello Kotlin"
+//fun getGreeting(): String = "Hello Kotlin"
+//
+//fun sayHello(): Unit {
+//    println(getGreeting())
+//}
 
-fun sayHello(): Unit {
-    println(getGreeting())
+fun sayHello(nameToGreet: String) {
+    val msg = "Hello " + nameToGreet
+    println(msg)
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
-    sayHello()
+//    println(getGreeting())
+    sayHello("Kotlin")
 }
