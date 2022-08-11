02fe6c7870a2727f7838427037446f497364514e
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-11 09:58:23 +0530

    val vs var

---
+++ Basics/src/Main.kt
@@ -0,0 +1,13 @@
+val name: String = "name"
+var string: String = "123"
+
+fun main(args: Array<String>) {
+    //                                                        Hello World
+    println("Hello Kotlin")
+    println(name)
+
+    //                                                        var vs val
+//    name = "abc"  // Error
+    string = "456"
+    println(string)
+}
--- src/Variables.kts
+++
@@ -1,9 +0,0 @@
-val name : String = "John"
-var greeting: String = "Hello"
-
-fun main() {
-    println(name)
-
-    greeting = "Hi"
-    println(greeting) // changed to Hi
-}
