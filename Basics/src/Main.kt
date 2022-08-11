140269fac09575cefe29f67836791b59c46e92bd
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-11 11:58:01 +0530

    if else and String concatenation

--- Basics/src/Main.kt
+++ Basics/src/Main.kt
@@ -1,13 +1,24 @@
-val name: String = "name"
-var string: String = "123"
+val name: String = "Kotlin"
+var greeting: String? = null
 
 fun main(args: Array<String>) {
-    //                                                        Hello World
-    println("Hello Kotlin")
-    println(name)
+//    //                                                        Hello World
+//    println("Hello Kotlin")
+//    println(name)
+//
+//    //                                                        var vs val
+//    //  name = "abc"  // Error
+//    string = "456"
+//    println(string)
 
-    //                                                        var vs val
-//    name = "abc"  // Error
-    string = "456"
-    println(string)
+//                                                              if else
+    if (greeting==null){
+        greeting = "Hi"
+    }else{
+        println("Greeting is not null")
+    }
+
+//                                                              String Concatenation
+    println("$greeting $name")
+
 }
