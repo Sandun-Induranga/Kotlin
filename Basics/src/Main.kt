5507bbe5fc4425185948375ef06c4bb9925ef535
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-11 23:25:39 +0530

    Content Updated

--- Basics/src/Main.kt
+++ Basics/src/Main.kt
@@ -1,1 +1,8 @@
+fun main() {
 
+    //                                                                  array
+    val programmingLanguages = arrayOf("Java","Python","Kotlin","C++")
+    println(programmingLanguages.size)
+    println(programmingLanguages[0])
+    println(programmingLanguages.get(0))
+}
